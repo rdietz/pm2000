28456317739d84585239e47f5e807e25eaa68d04
tgillett <tgillett@gmail.com>
2016-06-25 09:41:55 +1000

    Added git.h

--- PulseMaster2000/git.h
+++ PulseMaster2000/git.h
@@ -1,1 +1,5 @@
-SHA:  50df1633ed05efdb878d0012e25da0d88e42825f
+commit 50df1633ed05efdb878d0012e25da0d88e42825f
+Author: tgillett <tgillett@gmail.com>
+Date:   Sat Jun 25 09:30:13 2016 +1000
+
+    Updated schematic
--- git.h
+++
@@ -1,5 +0,0 @@
-commit 50df1633ed05efdb878d0012e25da0d88e42825f
-Author: tgillett <tgillett@gmail.com>
-Date:   Sat Jun 25 09:30:13 2016 +1000
-
-    Updated schematic
